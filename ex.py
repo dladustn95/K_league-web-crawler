dfef835a09bd5ed8f7e08fbfbf039b443a78ba6c
dladustn95 <dladustn95@gmail.com>
2020-03-19 09:52:30 +0900

    Change code, split text for each player

--- ex.py
+++ ex.py
@@ -1,6 +1,8 @@
 from pymongo import MongoClient
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 
 client = MongoClient('localhost', 27017)
@@ -12,8 +14,9 @@ test_collection = db.test_collection
 #test_collection.remove();
 #test_collection.insert({"team":"team1", "record":[{'round':1, 'where':'where1'},{"round":2, 'where':'where2'}]})
 
-
-driver = webdriver.Chrome('C:/Users/LYS/Desktop/K/python/chromedriver')
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(executable_path='D:/기타/k/chromedriver.exe', options=options)
 
 driver.get('http://data.kleague.com/')
 driver.get('http://portal.kleague.com/mainFrame.do')
@@ -23,8 +26,14 @@ inputElement = driver.find_element_by_link_text('데이터센터')
 inputElement.send_keys("\n") #send enter for links, buttons
 #driver.find_element_by_link_text('데이터센터').click()
 
-for i in range(1,28):
+select = Select(driver.find_element_by_name('meetYear'))
+select.select_by_value(str(2019))
+
+seqselect = Select(driver.find_element_by_name('meetSeq'))
+seqselect.select_by_value(str(1))
+for i in range(1,39):
     round = i;
+    print(round)
     select1 = Select(driver.find_element_by_name('roundId'))
     select1.select_by_value(str(round))
     select3 = Select(driver.find_element_by_name('gameId'))
@@ -43,47 +52,62 @@ for i in range(1,28):
         select3.select_by_value(str((round-1)*6 + 1 + i))
         match = driver.find_element_by_class_name('match-prame-boxTxt02')
         matchText = match.text.replace(" ","")
-        print(matchText)
 
         #팀 이름
         hometeam = driver.find_element_by_xpath('//*[@id="frm"]/div[2]/div[2]/span[1]')
-        print(hometeam.text)
         awayteam = driver.find_element_by_xpath('//*[@id="frm"]/div[3]/div[1]/span[3]')
-        print(awayteam.text)
 
         #홈팀 기록
+        #공격
         homeAttList = driver.find_element_by_xpath('//*[@id="homeAttList"]/tbody')
-        homeAttListText = str(homeAttList.text).replace('\n', ' ').split(' ')
+        homeAttListText = str(homeAttList.text).split('\n')
+        for idx, text in enumerate(homeAttListText):
+            homeAttListText[idx] = text.split(' ')
 
+        #패스
         inputElement = driver.find_element_by_xpath('//*[@id="btnPassFlag"]')
         inputElement.send_keys("\n")
         homePassList = driver.find_element_by_xpath('//*[@id="homePassList"]/tbody')
-        homePassListText = str(homePassList.text).replace('\n', ' ').split(' ')
+        homePassListText = str(homePassList.text).split('\n')
+        for idx, text in enumerate(homePassListText):
+            homePassListText[idx] = text.split(' ')
 
+        #수비
         inputElement = driver.find_element_by_xpath('//*[@id="btnDefFlag"]')
         inputElement.send_keys("\n")
         homeDefList = driver.find_element_by_xpath('//*[@id="homeDefList"]/tbody')
-        homeDefListText = str(homeDefList.text).replace('\n', ' ').split(' ')
+        homeDefListText = str(homeDefList.text).split('\n')
+        for idx, text in enumerate(homeDefListText):
+            homeDefListText[idx] = text.split(' ')
 
         driver.implicitly_wait(50);
 
         #원정팀 기록
+        #공격
         inputElement = driver.find_element_by_xpath('//*[@id="btnAwayTeam"]')
         inputElement.send_keys("\n")
         inputElement = driver.find_element_by_xpath('//*[@id="btnAttFlag"]')
         inputElement.send_keys("\n")
         awayAttList = driver.find_element_by_xpath('//*[@id="awayAttList"]/tbody')
-        awayAttListText = str(awayAttList.text).replace('\n', ' ').split(' ')
+        awayAttListText = str(awayAttList.text).split('\n')
+        for idx, text in enumerate(awayAttListText):
+            awayAttListText[idx] = text.split(' ')
 
+        #패스
         inputElement = driver.find_element_by_xpath('//*[@id="btnPassFlag"]')
         inputElement.send_keys("\n")
         awayPassList = driver.find_element_by_xpath('//*[@id="awayPassList"]/tbody')
-        awayPassListText = str(awayPassList.text).replace('\n', ' ').split(' ')
+        awayPassListText = str(awayPassList.text).split('\n')
+        for idx, text in enumerate(awayPassListText):
+            awayPassListText[idx] = text.split(' ')
 
+        #수비
         inputElement = driver.find_element_by_xpath('//*[@id="btnDefFlag"]')
         inputElement.send_keys("\n")
         awayDefList = driver.find_element_by_xpath('//*[@id="awayDefList"]/tbody')
-        awayDefListText = str(awayDefList.text).replace('\n',' ').split(' ')
+        awayDefListText = str(awayDefList.text).split('\n')
+        for idx, text in enumerate(awayDefListText):
+            awayDefListText[idx] = text.split(' ')
 
         #날씨
         weather = driver.find_element_by_xpath('//*[@id="frm"]/div[1]/div/ul/li[4]/span[2]')
@@ -93,39 +117,91 @@ for i in range(1,28):
         weatherText = weather[0]
         temperature = weather[1]
 
+        #print(len(homeAttListText))
+        #print(homeAttListText)
         #기록 DB에 저장
-        for i in range(0,int(len(homePassListText)/18)):
-            test_collection.update_one({'team': hometeam.text, 'name': homeAttListText[1 + i * 14], "backNum": homeAttListText[0 + i * 14], 'pos':homeAttListText[2+i*14]},
-                                    {'$push': {'record': {'round': round, 'matchTeam': awayteam.text, 'stadium': matchText, 'weather': weatherText,
-                                                         'temperature': temperature, 'mins': homeAttListText[3 + i * 14],'matchRate': homeAttListText[4 + i * 14],
-                                                'attRecord': {'goal': homeAttListText[5 + i * 14], 'sht': homeAttListText[6 + i * 14],  'sot': homeAttListText[7 + i * 14], 'paSht': homeAttListText[8 + i * 14],
-                                                            'paSot': homeAttListText[9 + i * 14], 'drb': homeAttListText[10 + i * 14], 'drbPer': homeAttListText[11 + i * 14],
-                                                            'f3FFouled': homeAttListText[12 + i * 14], 'f3In': homeAttListText[13 + i * 14] },
-                                                'passRecord': {'pass': homePassListText[5 + i * 18], 'passPer': homePassListText[6 + i * 18], 'keyP': homePassListText[7 + i * 18],
-                                                            'frontPass': homePassListText[8 + i * 18], 'backPass': homePassListText[9 + i * 18], 'attPass': homePassListText[10 + i * 18],
-                                                            'attPassPer': homePassListText[11 + i * 18], 'defPass': homePassListText[12 + i * 18], 'defPassPer': homePassListText[13 + i * 18],
-                                                            'longPass': homePassListText[14 + i * 18], 'longPassPer': homePassListText[15 + i * 18], 'cross': homePassListText[16 + i * 18],
-                                                            'crossPer': homePassListText[17 + i * 18]},
-                                                'defRecord': {'compete': homeDefListText[5 + i * 16], 'competePer': homeDefListText[6 + i * 16], 'air': homeDefListText[7 + i * 16],
-                                                            'airPer': homeDefListText[8 + i * 16], 'tackle': homeDefListText[9 + i * 16], 'tacklePer': homeDefListText[10 + i * 16],
-                                                            'clr': homeDefListText[11 + i * 16], 'inter': homeDefListText[12 + i * 16], 'foul': homeDefListText[13 + i * 16], 'yel': homeDefListText[14 + i * 16],
-                                                            'red': homeDefListText[15 + i * 16]}}}}, upsert = True)
-        for i in range(0,int(len(awayPassListText)/18)):
-            test_collection.update_one({'team': awayteam.text, 'name': awayAttListText[1 + i * 14], "backNum": awayAttListText[0 + i * 14],'pos': awayAttListText[2 + i * 14]},
-                                    {'$push': {'record': {'round': round, 'matchTeam': hometeam.text, 'stadium': matchText, 'weather': weatherText,
-                                                          'temperature': temperature, 'mins': awayAttListText[3 + i * 14], 'matchRate': awayAttListText[4 + i * 14],
-                                                'attRecord': {'goal': awayAttListText[5 + i * 14], 'sht': awayAttListText[6 + i * 14], 'sot': awayAttListText[7 + i * 14], 'paSht': awayAttListText[8 + i * 14],
-                                                             'paSot': awayAttListText[9 + i * 14], 'drb': awayAttListText[10 + i * 14], 'drbPer': awayAttListText[11 + i * 14],
-                                                             'f3FFouled': homeAttListText[12 + i * 14], 'f3In': homeAttListText[13 + i * 14]},
-                                                'passRecord': {'pass': awayPassListText[5 + i * 18], 'passPer': awayPassListText[6 + i * 18], 'keyP': awayPassListText[7 + i * 18],
-                                                            'frontPass': awayPassListText[8 + i * 18], 'backPass': awayPassListText[9 + i * 18], 'attPass': awayPassListText[10 + i * 18],
-                                                            'attPassPer': awayPassListText[11 + i * 18], 'defPass': awayPassListText[12 + i * 18], 'defPassPer': awayPassListText[13 + i * 18],
-                                                            'longPass': awayPassListText[14 + i * 18], 'longPassPer': awayPassListText[15 + i * 18], 'cross': awayPassListText[16 + i * 18],
-                                                            'crossPer': awayPassListText[17 + i * 18]},
-                                               'defRecord': {'compete': awayDefListText[5 + i * 16], 'competePer': awayDefListText[6 + i * 16], 'air': awayDefListText[7 + i * 16],
-                                                            'airPer': awayDefListText[8 + i * 16], 'tackle': awayDefListText[9 + i * 16], 'tacklePer': awayDefListText[10 + i * 16],
-                                                            'clr': awayDefListText[11 + i * 16], 'inter': awayDefListText[12 + i * 16], 'foul': awayDefListText[13 + i * 16], 'yel': awayDefListText[14 + i * 16],
-                                                            'red': awayDefListText[15 + i * 16]}}}}, upsert=True)
+        for idx, player in enumerate(homeAttListText):
+            if int(player[3]) > 0:
+                test_collection.update_one({'team': hometeam.text, 'name': player[1], "backNum": player[0]},
+                                           {'$push': {
+                                               'record': {'round': round, 'matchTeam': awayteam.text, 'stadium': matchText,
+                                                          'weather': weatherText, 'temperature': temperature,
+                                                          'pos':player[2], 'mins': player[3], 'matchRate': player[4],
+                                                          'attRecord': {'goal': homeAttListText[idx][5],
+                                                                        'sht': homeAttListText[idx][6],
+                                                                        'sot': homeAttListText[idx][7],
+                                                                        'paSht': homeAttListText[idx][8],
+                                                                        'paSot': homeAttListText[idx][9],
+                                                                        'drb': homeAttListText[idx][10],
+                                                                        'drbPer': homeAttListText[idx][11],
+                                                                        'f3In': homeAttListText[idx][12]},
+                                                          'passRecord': {'pass': homePassListText[idx][5],
+                                                                         'passPer': homePassListText[idx][6],
+                                                                         'keyP': homePassListText[idx][7],
+                                                                         'frontPass': homePassListText[idx][8],
+                                                                         'backPass': homePassListText[idx][9],
+                                                                         'attPass': homePassListText[idx][10],
+                                                                         'attPassPer': homePassListText[idx][11],
+                                                                         'defPass': homePassListText[idx][12],
+                                                                         'defPassPer': homePassListText[idx][13],
+                                                                         'longPass': homePassListText[idx][14],
+                                                                         'longPassPer': homePassListText[idx][15],
+                                                                         'cross': homePassListText[idx][16],
+                                                                         'crossPer': homePassListText[idx][17]},
+                                                          'defRecord': {'compete': homeDefListText[idx][5],
+                                                                        'competePer': homeDefListText[idx][6],
+                                                                        'air': homeDefListText[idx][7],
+                                                                        'airPer': homeDefListText[idx][8],
+                                                                        'tackle': homeDefListText[idx][9],
+                                                                        'tacklePer': homeDefListText[idx][10],
+                                                                        'clr': homeDefListText[idx][11],
+                                                                        'inter': homeDefListText[idx][12],
+                                                                        'foul': homeDefListText[idx][13],
+                                                                        'yel': homeDefListText[idx][14],
+                                                                        'red': homeDefListText[idx][15]}}}},
+                                           upsert=True)
+
+        for idx, player in enumerate(awayAttListText):
+            if int(player[3]) > 0:
+                test_collection.update_one({'team': awayteam.text, 'name': player[1], "backNum": player[0]},
+                                           {'$push': {
+                                               'record': {'round': round, 'matchTeam': hometeam.text,
+                                                          'stadium': matchText,
+                                                          'weather': weatherText, 'temperature': temperature,
+                                                          'pos': player[2], 'mins': player[3], 'matchRate': player[4],
+                                                          'attRecord': {'goal': awayAttListText[idx][5],
+                                                                        'sht': awayAttListText[idx][6],
+                                                                        'sot': awayAttListText[idx][7],
+                                                                        'paSht': awayAttListText[idx][8],
+                                                                        'paSot': awayAttListText[idx][9],
+                                                                        'drb': awayAttListText[idx][10],
+                                                                        'drbPer': awayAttListText[idx][11],
+                                                                        'f3In': awayAttListText[idx][12]},
+                                                          'passRecord': {'pass': awayPassListText[idx][5],
+                                                                         'passPer': awayPassListText[idx][6],
+                                                                         'keyP': awayPassListText[idx][7],
+                                                                         'frontPass': awayPassListText[idx][8],
+                                                                         'backPass': awayPassListText[idx][9],
+                                                                         'attPass': awayPassListText[idx][10],
+                                                                         'attPassPer': awayPassListText[idx][11],
+                                                                         'defPass': awayPassListText[idx][12],
+                                                                         'defPassPer': awayPassListText[idx][13],
+                                                                         'longPass': awayPassListText[idx][14],
+                                                                         'longPassPer': awayPassListText[idx][15],
+                                                                         'cross': awayPassListText[idx][16],
+                                                                         'crossPer': awayPassListText[idx][17]},
+                                                          'defRecord': {'compete': awayDefListText[idx][5],
+                                                                        'competePer': awayDefListText[idx][6],
+                                                                        'air': awayDefListText[idx][7],
+                                                                        'airPer': awayDefListText[idx][8],
+                                                                        'tackle': awayDefListText[idx][9],
+                                                                        'tacklePer': awayDefListText[idx][10],
+                                                                        'clr': awayDefListText[idx][11],
+                                                                        'inter': awayDefListText[idx][12],
+                                                                        'foul': awayDefListText[idx][13],
+                                                                        'yel': awayDefListText[idx][14],
+                                                                        'red': awayDefListText[idx][15]}}}},
+                                           upsert=True)
 
         driver.implicitly_wait(100);
 
